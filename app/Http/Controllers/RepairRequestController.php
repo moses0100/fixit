9465d424c263bc2a796d26b8128e3cb6d69bbf97
moses0100 <tanawatyadjantuek@gmail.com>
2026-09-26 20:59:04 +0700

    เบอร์ 10 หลัก

--- app/Http/Controllers/RepairRequestController.php
+++ app/Http/Controllers/RepairRequestController.php
@@ -43,7 +43,7 @@ class RepairRequestController extends Controller
             'title' => ['required', 'string', 'max:150'],
             'problem_description' => ['required', 'string', 'max:5000'],
             'urgency' => ['required', Rule::in(array_keys(RepairRequest::URGENCIES))],
-            'contact_phone' => ['required', 'string', 'regex:/^[0-9][0-9+() .-]{7,18}[0-9]$/'],
+            'contact_phone' => ['required', 'string', 'regex:/^0[0-9]{9}$/'],
             'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:2048'],
             'remove_image' => ['sometimes', 'boolean'],
         ], [
@@ -51,7 +51,7 @@ class RepairRequestController extends Controller
             'max' => ':attribute มีขนาดหรือความยาวเกินที่กำหนด',
             'image.image' => 'กรุณาแนบไฟล์รูปภาพ',
             'image.mimes' => 'รองรับเฉพาะ JPG, PNG และ WebP',
-            'contact_phone.regex' => 'กรุณากรอกเบอร์โทรศัพท์ 9–20 ตัวอักษร ขึ้นต้นและลงท้ายด้วยตัวเลข เช่น 0812345678',
+            'contact_phone.regex' => 'กรุณากรอกเบอร์มือถือ 10 หลัก ขึ้นต้นด้วย 0 เช่น 0812345678',
         ], ['brand' => 'ยี่ห้อ', 'title' => 'หัวข้อปัญหา', 'problem_description' => 'รายละเอียดอาการ', 'contact_phone' => 'เบอร์ติดต่อ', 'image' => 'รูปภาพ']);
     }
 
